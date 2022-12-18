c0533d8727f789313e6067c9a0ccd8e351569a47
Suheyb Coskun <suheyb.c@gmail.com>
2022-12-18 16:26:39 +0100

    add Layout component and styles

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { useMemo } from 'react';
+import './layout.css';
+
+const Layout: React.FC = ({ children }) => {
+  const currentYear = useMemo(() => new Date().getFullYear(), []);
+
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__title">Plan your trip!</h1>
+      </header>
+      <main className="main">{children}</main>
+      <footer className="footer">
+        <p className="footer__text">
+          <span className="footer__text__copyright">&copy;</span> 2014-
+          {currentYear} Tiqets Amsterdam
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
