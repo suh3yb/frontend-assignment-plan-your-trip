9242606f85cd21d77cdb2271249a8453fb528fd1
Suheyb Coskun <suheyb.c@gmail.com>
2022-12-18 16:29:13 +0100

    add Select component, styles and triangle image

---
+++ src/components/Select.tsx
@@ -0,0 +1,66 @@
+import React, { useCallback, useMemo } from 'react';
+import './select.css';
+
+interface Props {
+  label: string;
+  defaultOption: string;
+  options?: string[];
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Select: React.FC<Props> = ({
+  label,
+  defaultOption,
+  options,
+  setValue,
+}) => {
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setValue(event.target.value);
+    },
+    [setValue]
+  );
+
+  const mapOptionsToOptionList = useCallback(
+    () =>
+      options?.map((optionValue, index) => (
+        <option key={`option-${optionValue}-${index}`} value={optionValue}>
+          {optionValue}
+        </option>
+      )),
+    [options]
+  );
+
+  const selectionElemClass = useMemo(() => {
+    let className = 'selection';
+    if (!options) {
+      className += ' selection--disabled';
+    }
+
+    return className;
+  }, [options]);
+
+  const selectElemId = useMemo(
+    () => `${label}-${defaultOption}`,
+    [label, defaultOption]
+  );
+
+  return (
+    <div className={selectionElemClass}>
+      <label className="selection__label" htmlFor={selectElemId}>
+        {label}
+      </label>
+      <select
+        id={selectElemId}
+        className="selection__select"
+        defaultValue=""
+        onChange={handleChange}
+      >
+        <option value="">{defaultOption}</option>
+        {mapOptionsToOptionList()}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
